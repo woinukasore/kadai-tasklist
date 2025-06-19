6042eacdb7de242edf740cd44afd30fa84c6328a
root <root@xtci-240045316.trans-cosmos.co.jp>
2025-06-19 17:33:39 +0900

    first commit

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Task;
+
+class TasksController extends Controller
+{
+
+    // getでtasks/にアクセスされた場合の「一覧表示処理」
+    public function index()
+    {
+        // タスク一覧を取得
+        $tasks = Task::all();
+
+        // タスク一覧ビューでそれを表示
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    // getでtasks/createにアクセスされた場合の「新規登録画面表示処理」
+    public function create()
+    {
+        $task = new Task;
+
+        // タスク作成ビューを表示
+        return view('tasks.create', [
+            'task' => $task,
+        ]);
+    }
+
+    // getでtask/（任意のid）にアクセスされた場合の「取得表示処理」
+    public function show(string $id)
+    {
+        // idの値でタスクを検索して取得
+        $task = Task::findOrFail($id);
+
+        // タスク詳細ビューでそれを表示
+        return view('tasks.show', [
+            'task' => $task,
+        ]);
+    }
+
+    // getでtasks/（任意のid）/editにアクセスされた場合の「更新画面表示処理」
+    public function edit(string $id)
+    {
+        // idの値でタスクを検索して取得
+        $task = Task::findOrFail($id);
+
+        // タスク編集ビューでそれを表示
+        return view('tasks.edit', [
+            'task' => $task,
+        ]);
+    }
+
+    // postでtasks/にアクセスされた場合の「新規登録処理」
+    public function store(Request $request)
+    {
+        // タスクを作成
+        $task = new Task;
+        $task->content = $request->content;
+        $task->save();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+
+    // putまたはpatchでtasks/（任意のid）にアクセスされた場合の「更新処理」
+    public function update(Request $request, string $id)
+    {
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+        // メッセージを更新
+        $task->content = $request->content;
+        $task->save();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+
+    // deleteでmessages/（任意のid）にアクセスされた場合の「削除処理」
+    public function destroy(string $id)
+    {
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+        // メッセージを削除
+        $task->delete();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TasksController;
+
+Route::get('/', [TasksController::class, 'index']);
+Route::resource('tasks', TasksController::class);
